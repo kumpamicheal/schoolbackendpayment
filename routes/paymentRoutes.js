a3c30ef06db2fe43def53991b4c1ab8f599561a6
kumpamicheal <kumpamicheal@gmial.com>
2025-12-10 06:47:54 -0800

    SOLVING THE ROUTE PROLEM

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,19 +4,15 @@ const router = express.Router();
 // Import controller functions
 const paymentController = require("../controllers/paymentController");
 
-// Make sure these exist and are functions
-const { requestToPay, makePayment } = paymentController;
+// Destructure only the functions you have
+const { requestToPay } = paymentController;
 
-// Validate that imported handlers are functions
+// Validate that imported handler is a function
 if (typeof requestToPay !== "function") {
     throw new Error("requestToPay must be a function in paymentController");
 }
-if (typeof makePayment !== "function") {
-    throw new Error("makePayment must be a function in paymentController");
-}
 
 // Routes
-router.post("/request", requestToPay);   // Request payment
-router.post("/make", makePayment);       // Make payment
+router.post("/request", requestToPay); // Request payment
 
 module.exports = router;
